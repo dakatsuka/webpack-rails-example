efc7ff20419b826ce4283a120ecef1ce7b8e7695
Dai Akatsuka <d.akatsuka@gmail.com>
2016-06-08 15:38:37 +0900

    First commit

---
+++ app/frontend/javascripts/application.js
@@ -0,0 +1,15 @@
+/**
+ * Import Twitter bootstrap
+ */
+require('imports?jQuery=jquery!bootstrap-sass/assets/javascripts/bootstrap');
+require('font-awesome-sass-loader');
+
+/**
+ * Import stylesheet
+ */
+require('../stylesheets/application');
+
+/**
+ * Import images
+ */
+require('../images/sample.jpg');
